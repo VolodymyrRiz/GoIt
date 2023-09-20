f07597ba6cee59865c724a33650d31d108586b14
VolodymyrRiz <v.v.rizun@gmail.com>
2023-09-20 23:17:48 +0300

    main4.py_4

--- main4.py
+++ main4.py
@@ -1,3 +1,26 @@
-print("hello")
-print(5)
-print(6)
+borh = 0
+sum = None
+i = -1
+payment = list()
+dani = list()
+result = 0
+
+
+def amount_payment(payment):
+  
+    for sum in payment[:]:
+        global i
+        global result
+        sum = int(sum)
+        i = i + 1
+      
+        sum = sum + borh
+        #payment.insert(i, str(sum))
+        result = result + sum
+        print(sum)
+    print(result)
+    #return payment
+   
+
+amount_payment('10', '20', '30')
+pass
