6ebd45fc5f0fd64f46df83e00eba5d683705b412
VolodymyrRiz <v.v.rizun@gmail.com>
2023-09-23 22:29:31 +0300

    main4.py_6

--- main4.py
+++ main4.py
@@ -1,26 +1,58 @@
+# borh = None
+# sum = None
+# #i = -1
+# payment = ['10', '20', '30']
+# borh_list = ['1', '-3', '4']
+# #result = 0
+
+
+# def amount_payment(payment, borh_list):
+  
+#     for sum in payment[:] and borh in borh_list[:]:
+#         #global i
+#         #global result
+#         sum = int(sum)
+#         borh = int(borh)
+        
+#         #i = i + 1
+      
+#         sum = sum + borh
+#         #payment.insert(i, str(sum))
+#         #result = result + sum
+#         print(sum)
+#     #print(result)
+#     #return payment
+   
+
+# #amount_payment(['10', '20', '30', "1", "-3", "4"])
+
+# amount_payment(payment, borh_list)
+
 borh = 0
 sum = None
-i = -1
-payment = list()
-dani = list()
-result = 0
+#i = -1
+payment = ['a', 'b', 'c', 'd', 'e', 'f']
+#borh_list = ['d', 'e', 'f']
+#result = 0
 
 
 def amount_payment(payment):
   
-    for sum in payment[:]:
-        global i
-        global result
+    for sum in payment[0:5]:
+        #global i
+        #global result
+        
         sum = int(sum)
-        i = i + 1
+        borh = int(borh)
+        
+        #i = i + 1
       
         sum = sum + borh
         #payment.insert(i, str(sum))
-        result = result + sum
+        #result = result + sum
         print(sum)
-    print(result)
-    return payment
+    #print(result)
+    #return payment
    
 
-dani = dani('10', '20', '30')
-print(amount_payment(dani))
+amount_payment(['10', '20', '30', "1", "-3", "4"])
