ff52bca490f4ee2df93b69d03614f02aa22be969
VolodymyrRiz <v.v.rizun@gmail.com>
2023-09-24 23:47:41 +0300

    main4_2,py_5

--- main4_2.py
+++ main4_2.py
@@ -1,46 +1,39 @@
 args = ''
 item = [*args]
-recept = ''
+#recept = ''
 
 
-def format_ingredients(*item):
+def format_ingredients(item):
+
+
+    if item == None:
+        recept = None
+        return recept
     
     recept = ''
-    print(item)
     i = 0
-    i_len = len(*item)
-    print(i_len)
-    # if i_len == 1:
-    #     for komp in item:
-    #         recept = komp
-    #         print(recept)
-    #         return recept
-                
+    i_len = len(item)     
             
     for komp in item:
         i = i + 1
+        
         if i_len == 1:
-            print(komp)
             recept = f'{komp}'
             return recept
-        # if i_len == 2:
-        #     print(komp)
-        #     recept = f'{komp}'
-        #     return recept
-        if i == 1:
-            recept = f'{komp}' + ', '
-            print(recept)
+        if i == 1 and i_len != 2:
+             recept = f'{komp}' + ', '
+             continue
+        if i == 1 and i_len == 2:
+            recept = f'{komp}' + ' and '
             continue
         if i == i_len - 1:
             recept = recept + f'{komp}' + ' and '
             continue
         if i == i_len:
             recept = recept + f'{komp}'
-            print(recept)
             return recept
         else:
             recept = recept + f'{komp}' + ', '
-            print(recept)
-        
+                
          
-print(format_ingredients(['2 eggs', "1 liter sugar"]))
+print(format_ingredients(["www", 'eee']))
