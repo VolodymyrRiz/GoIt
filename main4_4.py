ec15ebca855f4fd594e8103a3d83fc7b9ad70ce2
VolodymyrRiz <v.v.rizun@gmail.com>
2023-09-25 16:44:00 +0300

    main4_4.py_2

--- main4_4.py
+++ main4_4.py
@@ -4,7 +4,7 @@ key = dict()
 def get_grade(key):
     key = {'F': 1, 'FX': 2, 'E': 3, 'D': 3, 'C': 4, 'B': 5, 'A': 5}
 
-    for key_select in key:
+    for key_select in key.values():
         print(key_select)
         #return key_select
     
@@ -13,13 +13,13 @@ def get_grade(key):
 
 def get_description(key):
     key = {
-        'F': Unsatisfactorily, 
-        'FX': Unsatisfactorily, 
-        'E': Enough, 
-        'D': Satisfactorily, 
-        'C': Good, 
-        'B': Very good, 
-        'A': Perfectly}
+        'F': 'Unsatisfactorily', 
+        'FX': 'Unsatisfactorily', 
+        'E': 'Enough', 
+        'D': 'Satisfactorily', 
+        'C': 'Good', 
+        'B': 'Very good', 
+        'A': 'Perfectly'}
     
         
         
