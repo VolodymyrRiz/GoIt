c4204bb8f9babbe49afea382eeb8f0660c5c600d
VolodymyrRiz <v.v.rizun@gmail.com>
2023-09-30 15:39:24 +0300

    main4_7_3

--- main4_7.py
+++ main4_7.py
@@ -2,40 +2,37 @@ points = {(0, 1): 2, (0, 2): 3.8, (0, 3): 2.7, (1, 2): 2.5, (1, 3): 4.1, (2, 3):
 
 def calculate_distance(coordinates):
     
-    points = {(0, 1): 2, (0, 2): 3.8, (0, 3): 2.7, (1, 2): 2.5, (1, 3): 4.1, (2, 3): 3.9}
+    #points = {(0, 1): 2, (0, 2): 3.8, (0, 3): 2.7, (1, 2): 2.5, (1, 3): 4.1, (2, 3): 3.9}
     
     res = 0
     count = 0
     len_coor = len(coordinates)
     
-    if coordinates == []:
+    if coordinates == ['']:
         return [0]
 
     for key in coordinates:
-        #print(key)
+        
         if count < len_coor - 1:
                         
             char = coordinates[count:count+2]
-            #print(char)
+            
             char_1 = char[0]
             char_2 = char[1]
             if char_1 > char_2:
                 char.reverse()
-                #print(char)
+                
             count += 1
             char = tuple(char)
-            #print(type(char))
             if char in points:
-                res_1 = points.pop(char)
+                res_1 = points.get(char)
                 
                 res = res + res_1
-                print(res)
-            
+                          
             else:
                 continue
-              
-
+ 
     return res
 
         
-calculate_distance([0, 1, 3, 2, 0, 2])
+print(calculate_distance([0, 1, 3, 2, 0, 2]))
