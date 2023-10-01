d4eebfbccc2eff630147274f8d3c379c90e64cee
VolodymyrRiz <v.v.rizun@gmail.com>
2023-10-01 15:57:28 +0300

    main4_11_3

--- main4_11.py
+++ main4_11.py
@@ -1,56 +1,43 @@
-#password = 'NmlDl1V0'
-#password = 'z,qrE*IE'
-password = ''
-false1 = False
-true1 = True
-flag = 0
 
-def search_method(char):
-    
-    global flag
-
-    #password = 'absdfrnt'
-    if char in password:
-        flag = 1
-        true1 = True
-        return true1
-    else:
-        false1 = False
-        return false1
 
 
 def is_valid_password(password):
+    
     big = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     small = 'abcdefghijklmnopqrstuvwxyz'
-    global flag
+    flag = 0
     number = '012345678'
     
     if len(password) == 8:
-        #print(len(password))
-        for char in big:
-            print(flag)
-            search_method(char)
-        if flag == 0:
-            return False
-        if false1 == False and flag == 1 and true1 == True:
-            flag = 0
-            for char in small:
-                search_method(char)
-            if flag == 0:
-                return False
-            if false1 == False and flag == 1 and true1 == True:
-                flag = 0
-                for char in number:
-                    
-                    search_method(char)
-                if flag == 0:
-                    return False
-                if false1 == False and flag == 1 and true1 == True:
-                    return True
-                
-          
+        pass
     else:
         return False
-            
-     
-print(is_valid_password('NmlDl1V0'))    
+
+    for char in big:
+        print(char)
+        if char in password:
+            flag = flag + 1
+            print(flag)
+    if flag == 0:
+        return False
+    else:
+        flag = 0
+        for char in small:
+            print(char)
+            if char in password:
+                flag = flag + 1
+                print(flag)
+        if flag == 0:
+            return False
+        else:
+            flag = 0
+            for char in number:
+                print(char)
+                if char in password:
+                    flag = flag + 1
+                    print(flag)
+            if flag == 0:
+                return False
+            else:
+                
+                return True
