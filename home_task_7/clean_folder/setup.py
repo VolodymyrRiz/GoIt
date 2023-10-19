891f82b76d50187ebb5f4521f3a69c167f818ac1
VolodymyrRiz <v.v.rizun@gmail.com>
2023-10-20 00:08:48 +0300

    clean_3

--- home_task_7/clean_folder/setup.py
+++ home_task_7/clean_folder/setup.py
@@ -2,8 +2,12 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name='clean_folder',
-    version='0.0.1',
+    version='0.0.1',    
+    description='Програма для впорядкування папки',
+    url='http://github.com/dummy_user/useful',
     author='Volodymyr Rizun',
+    author_email='v.v.rizun@gmail.com',
+    license='***',
     packages=find_namespace_packages(),
     entry_points={'console_scripts': ['clean=clean_folder.clean:start']}
 )
