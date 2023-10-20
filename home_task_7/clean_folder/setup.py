dae08eb3e8fc59d942a5fb19b683eb314da5de1a
VolodymyrRiz <v.v.rizun@gmail.com>
2023-10-20 12:44:36 +0300

    clean_4

--- home_task_7/clean_folder/setup.py
+++ home_task_7/clean_folder/setup.py
@@ -4,10 +4,10 @@ setup(
     name='clean_folder',
     version='0.0.1',    
     description='Програма для впорядкування папки',
-    url='http://github.com/dummy_user/useful',
+    url='https://github.com/VolodymyrRiz/GoIt/tree/main/home_task_7/clean_folder',
     author='Volodymyr Rizun',
     author_email='v.v.rizun@gmail.com',
-    license='***',
+    license='NNN',
     packages=find_namespace_packages(),
     entry_points={'console_scripts': ['clean=clean_folder.clean:start']}
 )
