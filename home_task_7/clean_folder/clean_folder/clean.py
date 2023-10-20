312604da3e285b0d7392980b9f92cde4022a6d42
VolodymyrRiz <v.v.rizun@gmail.com>
2023-10-20 14:19:23 +0300

    clean_7

--- home_task_7/clean_folder/clean_folder/clean.py
+++ home_task_7/clean_folder/clean_folder/clean.py
@@ -1,8 +1,90 @@
-from pathlib import Path
-import shutil
 import sys
-import file_parser
-from normalize import normalize
+import re
+import shutil
+from pathlib import Path
+
+JPEG_IMAGES = []
+JPG_IMAGES = []
+PNG_IMAGES = []
+SVG_IMAGES = []
+MP3_AUDIO = []
+WAV_AUDIO = []
+AVI_VIDEOS = []
+MP4_VIDEOS = []
+DOC_DOCUMENTS = []
+DOCX_DOCUMENTS = []
+XLS_DOCUMENTS = []
+PDF_DOCUMENTS = []
+MY_OTHER = []
+ZIP_ARCHIVES = []
+RAR_ARCHIVES = []
+
+REGISTER_EXTENSION = {
+    'JPEG': JPEG_IMAGES,
+    'JPG': JPG_IMAGES,
+    'PNG': PNG_IMAGES,
+    'SVG': SVG_IMAGES,
+    'MP3': MP3_AUDIO,
+    'ZIP': ZIP_ARCHIVES,
+    'PDF': PDF_DOCUMENTS,
+    'DOC': DOC_DOCUMENTS,
+    'DOCX': DOCX_DOCUMENTS,
+    'WAV': WAV_AUDIO,
+    'XLS': XLS_DOCUMENTS,
+    'RAR': RAR_ARCHIVES,
+    'AVI': AVI_VIDEOS,
+    'MP4': MP4_VIDEOS
+}
+
+FOLDERS = []
+EXTENSIONS = set()
+UNKNOWN = set()
+
+
+def get_extension(name: str) -> str:
+    return Path(name).suffix[1:].upper()  # suffix[1:] -> .jpg -> jpg
+
+def scan(folder: Path):
+    print("СКАНУЮ ПАПКИ ТА ФАЙЛИ. Зачекайте..........")
+    for item in folder.iterdir():
+        # Робота з папкою
+        if item.is_dir():  # перевіряємо чи обєкт папка
+            if item.name not in ('archives', 'video', 'audio', 'documents', 'images', 'MY_OTHER'):
+                FOLDERS.append(item)
+                scan(item)
+            continue
+
+        # Робота з файлом
+        extension = get_extension(item.name)  # беремо розширення файлу
+        full_name = folder / item.name  # беремо повний шлях до файлу
+        if not extension:
+            MY_OTHER.append(full_name)
+        else:
+            try:
+                ext_reg = REGISTER_EXTENSION[extension]
+                ext_reg.append(full_name)
+                EXTENSIONS.add(extension)
+            except KeyError:
+                UNKNOWN.add(extension)  # .mp4, .mov, .avi
+                MY_OTHER.append(full_name)
+
+
+CYRILLIC_SYMBOLS = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ"?*()+-%!№=@# '
+TRANSLATION = ("a", "b", "v", "g", "d", "e", "e", "j", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u",
+               "f", "h", "ts", "ch", "sh", "sch", "_", "y", "_", "e", "yu", "u", "ja", "je", "ji", "g", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_")
+
+TRANS = dict()
+
+for cyrillic, latin in zip(CYRILLIC_SYMBOLS, TRANSLATION):
+    TRANS[ord(cyrillic)] = latin
+    TRANS[ord(cyrillic.upper())] = latin.upper()
+
+
+def normalize(name: str) -> str:
+    print("ПЕРЕЙМЕНОВУЮ ПАПКИ ТА ФАЙЛИ З МЕТОЮ НОРМАЛІЗАЦІЇ НАЗВ........")
+    translate_name = re.sub(r'\W\[^.]', '_', name.translate(TRANS))
+    return translate_name
+
 
 def handle_media(file_name: Path, target_folder: Path):
     target_folder.mkdir(exist_ok=True, parents=True)
@@ -26,39 +108,39 @@ def handle_archive(file_name: Path, target_folder: Path):
 
 def main(folder: Path):
     print('ПОЧИНАЮ УПОРЯДКУВАННЯ ВАШОЇ ПАПКИ. Невідомі файли будуть у папці "MY_OTHER"')
-    file_parser.scan(folder)
-    for file in file_parser.JPEG_IMAGES:
+    scan(folder)
+    for file in JPEG_IMAGES:
         handle_media(file, folder / 'images' / 'JPEG')
-    for file in file_parser.JPG_IMAGES:
+    for file in JPG_IMAGES:
         handle_media(file, folder / 'images' / 'JPG')
-    for file in file_parser.PNG_IMAGES:
+    for file in PNG_IMAGES:
         handle_media(file, folder / 'images' / 'PNG')
-    for file in file_parser.SVG_IMAGES:
+    for file in SVG_IMAGES:
         handle_media(file, folder / 'images' / 'SVG')        
-    for file in file_parser.AVI_VIDEOS:
+    for file in AVI_VIDEOS:
         handle_media(file, folder / 'videos' / 'AVI')
-    for file in file_parser.MP4_VIDEOS:
+    for file in MP4_VIDEOS:
         handle_media(file, folder / 'videos' / 'MP4')        
-    for file in file_parser.MP3_AUDIO:
+    for file in MP3_AUDIO:
         handle_media(file, folder / 'audio' / 'MP3_AUDIO')
-    for file in file_parser.WAV_AUDIO:
+    for file in WAV_AUDIO:
         handle_media(file, folder / 'audio' / 'WAV_AUDIO')         
-    for file in file_parser.DOC_DOCUMENTS:
+    for file in DOC_DOCUMENTS:
         handle_doc(file, folder / 'documents' / 'DOC')  
-    for file in file_parser.DOCX_DOCUMENTS:
+    for file in DOCX_DOCUMENTS:
         handle_doc(file, folder / 'documents' / 'DOCX')   
-    for file in file_parser.PDF_DOCUMENTS:
+    for file in PDF_DOCUMENTS:
         handle_doc(file, folder / 'documents' / 'PDF') 
-    for file in file_parser.XLS_DOCUMENTS:
+    for file in XLS_DOCUMENTS:
         handle_doc(file, folder / 'documents' / 'XLS')        
-    for file in file_parser.MY_OTHER:
+    for file in MY_OTHER:
         handle_media(file, folder / 'MY_OTHER')
-    for file in file_parser.ZIP_ARCHIVES:
+    for file in ZIP_ARCHIVES:
         handle_archive(file, folder / 'ARCHIVES')
-    for file in file_parser.RAR_ARCHIVES:
+    for file in RAR_ARCHIVES:
         handle_archive(file, folder / 'ARCHIVES')
 
-    for folder in file_parser.FOLDERS[::-1]:
+    for folder in FOLDERS[::-1]:
         # Видаляємо пусті папки після сортування
         try:
             folder.rmdir()
