2368394c24879b8c42a4896060f8e79543b93288
VolodymyrRiz <v.v.rizun@gmail.com>
2023-10-26 23:35:40 +0300

    test_5

--- home_task_8/test.py
+++ home_task_8/test.py
@@ -4,8 +4,8 @@ user_week = {'Monday': [],
              'Wednesday': [], 
              'Thursday': [], 
              'Friday': [], 
-             'Saturday': [],
-             'Sunday': [],
+             #'Saturday': [],
+             #'Sunday': [],
              }
 
 users = [
@@ -15,8 +15,8 @@ users = [
         {"name": "Jan Forest", "birthday": datetime(1978, 12, 5).date()},#
         {"name": "Jan Tard", "birthday": datetime(1980, 11, 3).date()},#
         {"name": "Jan Weis", "birthday": datetime(1990, 11, 4).date()},#
-        {"name": "Jan Flint", "birthday": datetime(1972, 10, 31).date()},#
-        {"name": "Jan Boss", "birthday": datetime(1976, 10, 27).date()},#
+        {"name": "Jan Flint", "birthday": datetime(1972, 10, 23).date()},#
+        {"name": "Jan Boss", "birthday": datetime(1976, 10, 30).date()},#
         {"name": "Jan Grape", "birthday": datetime(1977, 11, 1).date()},#
         {"name": "Jan Bad", "birthday": datetime(1977, 11, 5).date()},#
     ]
@@ -36,9 +36,53 @@ date = date_now.strftime("%d")
 
 for day in range(day_now, 7):
     #print(day)
+    
+    
     if day == 0:
-        name = users.get('name')
-        #print(name)
+        #end_of_week = 7 - day_now #4
+        #end_date_week = int(date) + end_of_week - 2 #27
+        #print(end_date_week)
+        day_next = 5
+        #print(day_next)
+            
+        for dict in users:
+            #print(dict)        
+            
+            for key, value in dict.items():
+                pass
+                #print(type(value))
+            value_month = value.strftime('%m')
+            #print('   ', value_month)
+            value_date_next = value.strftime('%d')
+            #print(value_date_next)
+            if day_next == 5:
+                day_next_word = 'Monday'
+            if day_next == 6:
+                day_next_word = 'Monday'
+           
+            #print(day_next_word)
+            #print(value_day)
+            #print(str(day_now))
+            #print(type(value)) 
+            #print(';;;;;;', day_next)
+            if value_month == month and day_next == 5 and int(value_date_next) == int(date) - 2: 
+                           
+                value = dict.get('name')
+                key_dict.append(value)  
+                #print('iiiiiiiiiiiiiiiiiiiiiiii')
+                #print(key_dict)  
+                user_week.update({day_next_word: key_dict}) 
+                #count_day += 1     
+                day_next = 6   
+                #value_true = value
+            if value_month == month and day_next == 6 and int(value_date_next) == int(date) - 1: 
+                             
+                value = dict.get('name')
+                key_dict.append(value)  
+                #print(key_dict)    
+                user_week.update({day_next_word: key_dict}) 
+            
+                
     elif day == day_now:
         for dict in users:
             #print(dict)
@@ -57,15 +101,15 @@ for day in range(day_now, 7):
                 key_dict.append(value)    
                 user_week.update({day_now_word: key_dict})  
                       
-                #print(user_week)
-                
+                #print(user_week)           
     
               
     end_of_week = 7 - day_now #4
     end_date_week = int(date) + end_of_week - 2 #27
     #print(end_date_week)
     day_next = day_now + count_day    #4
-    
+    #print(day_next)
+        
     for dict in users:
         #print(dict)        
           
@@ -73,11 +117,18 @@ for day in range(day_now, 7):
             pass
             #print(type(value))
         value_month = value.strftime('%m')
-        print(value_month)
+        #print(value_month)
         value_date_next = value.strftime('%d')
-        print(value_date_next)
-        day_next_word = value.strftime('%A')
-        print(day_next_word)
+        #print(value_date_next)
+        if day_next == 1:
+            day_next_word = 'Tuesday'
+        if day_next == 2:
+            day_next_word = 'Wednesday'
+        if day_next == 3:
+            day_next_word = 'Thursday'
+        if day_next == 4:
+            day_next_word = 'Friday'
+        #print(day_next_word)
         #print(value_day)
         #print(str(day_now))
         #print(type(value))  
