f743d38b69d09fdcaa6f625a34c89d34924b16b6
VolodymyrRiz <v.v.rizun@gmail.com>
2023-10-28 14:07:09 +0300

    com_13

--- home_task_8/main.py
+++ home_task_8/main.py
@@ -10,9 +10,14 @@ user_week = {'Monday': [],
 
 def get_birthdays_per_week(users):
     
-    key_dict = []
-        
-    date_now = datetime(year=2023, month=10, day=30)   
+    
+    # b = "Робити календар днів народжень із сьогоднішнього дня чи з наступного понеділка?\n"
+    # c = 'З наступного понеділка!'
+    # print(b, c)
+    # print(input())
+    
+    key_dict = []        
+    date_now = datetime(year=2023, month=5, day=1)   
     #date_now = date.today()
     
     day_now = date_now.weekday()
@@ -29,45 +34,63 @@ def get_birthdays_per_week(users):
         print(a)
         return user_week
         
-    for day in range(day_now, 5):        
+    for day_ in range(day_now, 5):        
         key_dict = []               
-        day_next = day
+        day_next = day_
         
         
      # Обробка вихідних днів   
-        if day == 0:
+        if day_ == 0:
                           
             for dict in users:                   
                 
                 for key, value in dict.items():
                     pass
-                   
-                value_month = value.strftime('%m')
                 
+                same_day = value.day
+                same_month = value.month
+                print(value.year)
+                now_year = date_now.year
+                print(now_year)
+                actual_datetime = datetime(now_year, same_month, same_day).date()
+                value = actual_datetime
+                print(value)
+                   
+                value_month = value.strftime('%m')                
                 value_date_next = value.strftime('%d')
-                if int(value_date_next) == int(date_) - 2:
-                   
+                #date_prev = value.strftime("%d")
+                day_prev = value.weekday()
+                
+                if (int(value_date_next) == 31 or int(value_date_next) == 30) and int(value_month) == int(month) - 1:
                     day_next_word = 'Monday'
-                if int(value_date_next) == int(date_) - 1:
-                  
+                    value = dict.get('name')
+                    key_dict.append(value)                      
+                    user_week.update({day_next_word: key_dict}) 
+                    
+                if int(value_date_next) == 29 and int(value_month) == int(month) - 1 and day_prev == 5:
+                    day_next_word = 'Monday'
+                    value = dict.get('name')
+                    key_dict.append(value)                      
+                    user_week.update({day_next_word: key_dict})    
+                    
+                    
+                if int(value_date_next) == int(date_) - 2:                   
+                    day_next_word = 'Monday'
+                if int(value_date_next) == int(date_) - 1:                  
                     day_next_word = 'Monday'
                 
-                if value_month == month and int(value_date_next) == int(date_) - 2: 
-                           
+                if value_month == month and int(value_date_next) == int(date_) - 2:                            
                     value = dict.get('name')
-                    key_dict.append(value)  
-                    
+                    key_dict.append(value)                      
                     user_week.update({day_next_word: key_dict}) 
                    
-                if value_month == month and int(value_date_next) == int(date_) - 1: 
-                            
+                if value_month == month and int(value_date_next) == int(date_) - 1:                             
                     value = dict.get('name')
-                    key_dict.append(value)  
-                       
+                    key_dict.append(value)                         
                     user_week.update({day_next_word: key_dict}) 
             
      # Обробка актуального дня               
-        if day == day_now:
+        if day_ == day_now:
             
             for dict in users:
                                 
@@ -177,10 +200,10 @@ def get_birthdays_per_week(users):
 
 if __name__ == "__main__":
     users = [
-        {"name": "Jan Koum", "birthday": datetime(1976, 10, 1).date()},
-        {"name": "Jan Grot", "birthday": datetime(1957, 2, 1).date()},
-        {"name": "Jan Swift", "birthday": datetime(1977, 2, 5).date()},
-        {"name": "Jan Forest", "birthday": datetime(1978, 2, 5).date()},
+        {"name": "Jan Koum", "birthday": datetime(1976, 9, 30).date()},
+        {"name": "Jan Grot", "birthday": datetime(1957, 10, 1).date()},
+        {"name": "Jan Swift", "birthday": datetime(1977, 3, 30).date()},
+        {"name": "Jan Forest", "birthday": datetime(1978, 3, 29).date()},
         {"name": "Jan Tard", "birthday": datetime(1980, 10, 14).date()},
         {"name": "Jan Weis", "birthday": datetime(1972, 10, 15).date()},
         {"name": "Jan Flint", "birthday": datetime(1972, 10, 16).date()},
