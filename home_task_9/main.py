00ad0f18a9111099fb402202acfba0c3e4c5e6b0
VolodymyrRiz <v.v.rizun@gmail.com>
2023-10-29 17:42:49 +0200

    dz9_1

--- home_task_9/main.py
+++ home_task_9/main.py
@@ -1,1 +1,26 @@
 # ДЗ9
+# "hello", відповідає у консоль "How can I help you?"
+
+def hello(txt):
+    return txt
+
+def add(txt):
+    return txt
+
+def main(prin_inp):
+    
+    print(a)
+    d = input()
+    if d == 'hello':
+        print('How can I help you?')ff
+    return
+
+a = '''
+My name is SYSTEM BOT. 
+I can help you to work with contacts and their phone numbers.
+Are you ready to communicate? 
+If you are ready tell me HELLO (print please)
+'''
+while True:
+    main(a)
+
