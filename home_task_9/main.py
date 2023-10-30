241a9bdac17f096722da483ebef1928c47c332bb
VolodymyrRiz <v.v.rizun@gmail.com>
2023-10-30 17:55:12 +0200

    dz9_4

--- home_task_9/main.py
+++ home_task_9/main.py
@@ -2,6 +2,7 @@
 # "hello", відповідає у консоль "How can I help you?"
 
 import os
+calc = 0
 
 def hello(txt):
     return txt
@@ -9,16 +10,26 @@ def hello(txt):
 def add(txt):
     return txt
 
+
 def main(prin_inp):
-    
-       
+    global calc
+    calc += 1   
+    print(calc)
     print(a)
+    if calc == 10:
+        print("Don't make fun of me, 5 more mistakes and I'm done")
+    if calc == 15:
+        print('Good bye!!!')        
+        return os.abort()
+    
     d = input()
     
     if d == 'hello' or d == 'Hello' or d == 'HELLO':
            
         print('How can I help you?')
         d = input()
+        
+        
     elif d == '.':
         print('Good bye. See you later!')        
         return os.abort()
