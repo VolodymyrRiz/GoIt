f2c27b235cc204c52ddf2becfbbe32a48deb2752
VolodymyrRiz <v.v.rizun@gmail.com>
2024-02-07 21:52:09 +0200

    yyyy

--- home_task_10/check_homework.py
+++ home_task_10/check_homework.py
@@ -8,7 +8,7 @@
 from collections import UserDict
 import unittest
 
-import main
+import personal_bot.main as main
 
 
 RED = "\033[91m"
--- home_task_8/check_homework.py
+++ home_task_8/check_homework.py
@@ -9,7 +9,7 @@ import unittest
 from datetime import datetime, timedelta
 from unittest.mock import patch
 
-from main import get_birthdays_per_week
+from personal_bot.main import get_birthdays_per_week
 
 RED = "\033[91m"
 GREEN = "\033[92m"
